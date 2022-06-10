0a520136d7cec905b59330f835cce81b7f34552d
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-06-10 20:31:41 +0100

    first use of react-query

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,21 @@
+import axios from "axios"
+import { useQuery } from "react-query"
+
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
+  const { isLoading, isFetching, data, error } = useQuery('get-heros', () => { return axios.get('http://localhost:4000/superheroes') })
+  console.log({
+    isLoading,
+    isFetching
+  });
+  return (
+    <div>
+      <h2>React Query Super Heroes Page</h2>
+      {isLoading && <h2>Loading...</h2>}
+      {isFetching && <h2>Fetching...</h2>}
+
+      {error && <h2>Error was found : {error}</h2>}
+      {JSON.stringify(data)}
+    </div>
+  )
 }
