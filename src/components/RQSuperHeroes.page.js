49d730c9bbbc002b9d04748243b73e58f3981d21
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-06-10 23:15:34 +0100

    error msg

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,21 +1,28 @@
-import axios from "axios"
-import { useQuery } from "react-query"
-
+import axios from "axios";
+import { useQuery } from "react-query";
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, isFetching, data, error } = useQuery('get-heros', () => { return axios.get('http://localhost:4000/superheroes') })
-  console.log({
-    isLoading,
-    isFetching
+  const { isLoading, isFetching, data, isError, error } = useQuery("get-heros", () => {
+    return axios.get("http://localhost:4000/superheroes1");
   });
+  const result = useQuery("get-heros", () => {
+    return axios.get("http://localhost:4000/superheroes1");
+  });
+
+  console.log(result);
+  console.log({ error });
+
+
   return (
     <div>
       <h2>React Query Super Heroes Page</h2>
       {isLoading && <h2>Loading...</h2>}
+
       {isFetching && <h2>Fetching...</h2>}
 
-      {error && <h2>Error was found : {error}</h2>}
+      {isError && <h2>Error was found : {error?.message}</h2>}
+
       {JSON.stringify(data)}
     </div>
-  )
-}
+  );
+};
