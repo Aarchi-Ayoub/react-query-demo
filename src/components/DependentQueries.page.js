327224a6d99986ae6ef834708b62d8876baf5db0
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-06-24 18:30:39 +0100

    Dependent Queries

--- src/components/DependentQueries.page.js
+++ src/components/DependentQueries.page.js
@@ -1,5 +1,24 @@
+import axios from "axios";
 import React from "react";
+import { useQuery } from "react-query";
 
-export function DependentQueries() {
+// Geting users
+const getUser = (id) => {
+  return axios.get(`http://localhost:4000/users/${id}`);
+};
+
+// Geting jobs
+const getJobs = (id) => {
+  return axios.get(`http://localhost:4000/jobs/${id}`);
+};
+
+export function DependentQueries({ email }) {
+  const { data: user } = useQuery(["user", email], () => getUser(email));
+
+  const userID = user?.data?.name;
+  const { data: jobs } = useQuery(["jobs", userID], () => getJobs(userID), {
+    enabled: !!userID,
+  });
+
   return <div>DependentQueries</div>;
 }
